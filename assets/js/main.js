e73a642361d8e05e2aac6b7bf2ebed15cc9c0fdb
Joned Wedus <lumbantoruanebin@gmail.com>
2021-07-22 12:44:02 +0700

    Certificate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     // Show up navbar
     const media = $('#media').children('div').hasClass('media1');
     $(window).scroll(function () {
@@ -9,13 +10,60 @@ $(document).ready(function () {
             $('.navbar').removeClass('sticky');
             $('.menu-btn').removeClass('sticky');
         }
-    })
+    });
     // Menu Button toggle
     if(media){
         $('.menu-btn').click(function () {
             $('.navbar .menu').toggleClass('active');
             $('.media1').toggleClass('change');
             $('.media1 i').toggleClass('active');
+            
+            const active = $('.navbar .menu').hasClass('active')
+            if (active){
+                // Menu Button Item Toggle
+                $('.menu .a').click(function () {
+                    $('.navbar .menu').removeClass('active');
+                    $('.media1').removeClass('change');
+                    $('.media1 i').removeClass('active');
+                })
+            }
         })
-    }
+        
+        
+    };
+    // Typing Animation
+    let typed1 = new Typed(".typing1",{
+        strings: ["Developer", "Student", "Engineer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+    let typed2 = new Typed(".typing2",{
+        strings: ["Engineer", "Developer", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    // Owl Carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            730:{
+                items: 2,
+                nav: false
+            },
+            908:{
+                items: 3,
+                nav: false
+            }
+        }
+    })
 })
